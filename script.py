f22ff7a5f257e951343aa4d3f7d20109cdf9f80d
La Tasha Pollard <latashajpollard@gmail.com>
2025-03-05 17:06:48 -0600

    format

--- script.py
+++ script.py
@@ -14,7 +14,8 @@ TARGET_GROUP_NAME = "sports-api-tg"
 HEALTH_CHECK_PATH = "/sports"
 IMAGE_TAG = "sports-api-latest"
 EXECUTION_ROLE_ARN = "arn:aws:iam::443370693600:role/ecsTaskExecutionRole"
-SUBNETS = ["subnet-023863d2535821480", "subnet-0eeb018c552f1801c", "subnet-072732b43c768c4fb"]
+SUBNETS = ["subnet-023863d2535821480",
+           "subnet-0eeb018c552f1801c", "subnet-072732b43c768c4fb"]
 
 ecr_client = boto3.client('ecr', region_name=AWS_REGION)
 ecs_client = boto3.client('ecs', region_name=AWS_REGION)
@@ -22,6 +23,8 @@ elb_client = boto3.client('elbv2', region_name=AWS_REGION)
 api_gateway_client = boto3.client('apigateway', region_name=AWS_REGION)
 
 # If it doens't exist, create ECR Repo and verify creation
+
+
 def create_ecr_repo():
     try:
         response = ecr_client.create_repository(repositoryName=ECR_REPO_NAME)
@@ -39,30 +42,37 @@ def create_ecr_repo():
         exit(1)
 
 # Log into ECR
+
+
 def login_to_ecr():
     try:
         print("Logging into ECR...")
         token = ecr_client.get_authorization_token(
         )["authorizationData"][0]["authorizationToken"]
         registry_url = f"{AWS_ACCOUNT_ID}.dkr.ecr.{AWS_REGION}.amazonaws.com"
-        subprocess.run(f"aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin {registry_url}", check=True, text=True, shell=True)
+        subprocess.run(
+            f"aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin {registry_url}", check=True, text=True, shell=True)
         return registry_url
     except subprocess.CalledProcessError as e:
-      print(f"Error during Docker login: {e.stderr}")
-      exit(1)
+        print(f"Error during Docker login: {e.stderr}")
+        exit(1)
     except Exception as e:
         print(f"An error occurred trying to log into ECR: {str(e)}")
         exit(1)
 
 # Build, push, and tag Docker image
+
+
 def build_and_push_docker_image(repository_uri):
     try:
         print("Building Docker image...")
-        subprocess.run(f"docker build --platform linux/amd64 -t {ECR_REPO_NAME} .", shell=True, check=True)
+        subprocess.run(
+            f"docker build --platform linux/amd64 -t {ECR_REPO_NAME} .", shell=True, check=True)
 
         print("Tagging Docker image...")
         tagged_image = f"{repository_uri}:{IMAGE_TAG}"
-        subprocess.run(f"docker tag {ECR_REPO_NAME}:latest {tagged_image}", shell=True, check=True)
+        subprocess.run(
+            f"docker tag {ECR_REPO_NAME}:latest {tagged_image}", shell=True, check=True)
 
         print("Pushing Docker image...")
         subprocess.run(f"docker push {tagged_image}", shell=True, check=True)
@@ -73,6 +83,8 @@ def build_and_push_docker_image(repository_uri):
         exit(1)
 
 # Create ECS Cluster
+
+
 def create_ecs_cluster():
     response = ecs_client.list_clusters()
     if ECS_CLUSTER_NAME in response["clusterArns"]:
@@ -88,6 +100,8 @@ def create_ecs_cluster():
             exit(1)
 
 # Register Task Definition
+
+
 def register_task_definition(repository_uri):
     try:
         response = ecs_client.list_task_definitions(
@@ -118,19 +132,24 @@ def register_task_definition(repository_uri):
                     }
                 ]
             )
-            print(f"Task Definition in Family: {TASK_DEF_FAMILY} registered successfully.") 
+            print(
+                f"Task Definition in Family: {TASK_DEF_FAMILY} registered successfully.")
             return response["taskDefinition"]["taskDefinitionArn"]
     except Exception as e:
-        print(f"An error occurred during task definition registration: {str(e)}")
+        print(
+            f"An error occurred during task definition registration: {str(e)}")
         exit(1)
 
 # Create ECS Service
+
+
 def create_ecs_service(task_definition_arn):
-    services = ecs_client.list_services(cluster=ECS_CLUSTER_NAME)["serviceArns"]
+    services = ecs_client.list_services(
+        cluster=ECS_CLUSTER_NAME)["serviceArns"]
     if SERVICE_NAME in services:
         print(f'ECS Service: {SERVICE_NAME} already exists')
         return
-    else: 
+    else:
         print("Creating ECS Service...")
         response = ecs_client.create_service(
             cluster=ECS_CLUSTER_NAME,
@@ -145,8 +164,6 @@ def create_ecs_service(task_definition_arn):
                 }
             }
         )
-
-    
 
 
 print("Starting deployment...")
